0ea386d4e27ccaa81d932e5fd751288895622040
LeoUnglaub <leo@leo-unglaub.net>
2010-01-19 00:06:02 +0000

    -add the complete extension to the svn server -add the 2.8 compatibility
    git-svn-id: http://svn.lu-hosting.net/login_count@2 8049b1e7-ebc0-4768-b373-984406a9ab92

---
+++ trunk/system/modules/login_count/login_count.php
@@ -0,0 +1,47 @@
+<?php //if (!defined('TL_ROOT')) die('You can not access this file directly!');
+
+/**
+ * TYPOlight webCMS
+ * Copyright (C) 2005-2009 Leo Feyer
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at http://www.gnu.org/licenses/.
+ *
+ * PHP version 5
+ * @copyright  LU-Hosting 2009
+ * @author     Leo Unglaub <leo@leo-unglaub.net>
+ * @package    login_count
+ * @license    LGPL
+ * @filesource
+ */
+
+class login_count extends Frontend {
+	
+	public function count_fe($objUser)
+	{
+		$this->import('Database');
+		$objUserActCount = $this->Database->prepare('SELECT lu_login_count FROM tl_member WHERE id=?')->execute($objUser->id);
+		$intActCount = $objUserActCount->lu_login_count;
+		
+		//TODO: Ich weiß nicht obs nötig ist oder ob PHP mittlerweile auch null ++$value rechnen kann ohne notice.
+		if (empty($intActCount)){
+			$intActCount = 0;
+		}
+
+		$objUpdateCount = $this->Database->prepare('UPDATE tl_member %s WHERE id=?')
+								->set(array('lu_login_count' => ++$intActCount))
+								->execute($objUser->id);								
+	}
+}
+?>
